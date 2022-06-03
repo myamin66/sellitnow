82445e45cdc8a383fcf269221b7301cf34b29b00
myamin66 <myamin.bcss19@iba-suk.edu.pk>
2022-06-03 10:24:21 +0500

    More React Components

--- SellItNow/App.js
+++ SellItNow/App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Image } from 'react-native';
 
 export default function App() {
+  const handlePressEvent = () => {console.log('Press Event')};
   return (
-    <View style={styles.container}>
-
-      <Text>Hello World Android bro, native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Image source={require('./assets/favicon.png')}/> */}
+      <Image 
+      // blurRadius={5} 
+      fadeDuration = {1000}
+      source={{
+        width: 200,
+        height: 300,
+        uri: 'https://picsum.photos/200/300'}}/>
+      
+      <Text onPress={handlePressEvent}>Hello World Android bro, native</Text>
+      
+    </SafeAreaView>
   );
 }
 
