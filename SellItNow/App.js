a6c39ecf32c29afc9783e8ce6d740baa7e4bbb0a
myamin66 <myamin.bcss19@iba-suk.edu.pk>
2022-06-03 13:24:26 +0500

    layouts and other controls

--- SellItNow/App.js
+++ SellItNow/App.js
@@ -1,22 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView, Image } from 'react-native';
-
+// import { TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback } from 'react-native';
+import { Button, Alert } from 'react-native';
+import { Platform, Dimensions } from 'react-native';
 export default function App() {
+  // console.log(Dimensions.get('screen'));
+  // console.log(Dimensions.get('window'));
   const handlePressEvent = () => {console.log('Press Event')};
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Image source={require('./assets/favicon.png')}/> */}
-      <Image 
-      // blurRadius={5} 
-      fadeDuration = {1000}
-      source={{
-        width: 200,
-        height: 300,
-        uri: 'https://picsum.photos/200/300'}}/>
-      
-      <Text onPress={handlePressEvent}>Hello World Android bro, native</Text>
-      
+      <View style={{height: '30%', width:'100%', backgroundColor: 'dodgerblue'}}></View>
     </SafeAreaView>
   );
 }
@@ -26,6 +20,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'pink',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
+
 });       
